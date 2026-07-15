af551f67c8e3c7cf208836fb1f6bdc58ca4d5f9f
negociosmilionarios728-ai <negociosmilionarios728@gmail.com>
2026-07-15 20:25:46 -0300

    Update checkout link to Zuckpay

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const checkoutUrl = "https://pay.kiwify.com/NbNlVty";
+const checkoutUrl = "https://zuckpay.com.br/checkout/1000-recetas-para-airfryer";
 
 document.querySelectorAll(".cta-link").forEach((link) => {
   link.href = checkoutUrl;
